8fa4768d1eceba3ba0361d8843f301f0f1b5419c
anitavttcs <anita.vertetics@hotmail.com>
2021-04-30 12:39:56 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import "./main.css";
+//import { Modal, Button } from "react-bootstrap";
+import React, { useState } from "react";
+//import "bootstrap/dist/css/bootstrap.min.css";
+import Menu from "./components/Menu";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+
+      <div className="container">
+        <h1>Jelentkezés</h1>
+        <input type="text" placeholder="Név" />
+        <input type="email" placeholder="Email cím" />
+        <input type="tel" placeholder="Telefonszám" />
+        <div>
+          <input type="checkbox" /> Elolvastam és elfogadom az <span>Adatkezelési tájékoztatót.</span>
+        </div>
+
+        <button onClick={() => handleShow()}>ELKÜLD</button>
+
+
+        {/* <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>This is a Bootstrap modal</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+          </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+          </Button>
+          </Modal.Footer>
+        </Modal> */}
+      </div>
+
     </div>
   );
 }
